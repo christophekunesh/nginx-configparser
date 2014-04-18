1df82c089eecb6ed75e656e467fcf5353fb45b7b
Christophe Kunesh <christophekunesh@Christophes-MacBook-Pro.local>
2014-04-18 15:09:24 -0700

    Adding James tests

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -1,11 +1,41 @@
+#include <string.h>
+
 #include "gtest/gtest.h"
 #include "config_parser.h"
 
 TEST(NginxConfigParserTest, SimpleConfig) {
-  NginxConfigParser parser;
-  NginxConfig out_config;
-
-  bool success = parser.Parse("example_config", &out_config);
-
-  EXPECT_TRUE(success);
+    NginxConfigParser parser;
+    NginxConfig out_config;
+    
+    bool success = parser.Parse("example_config", &out_config);
+    
+    EXPECT_TRUE(success);
 }
+
+TEST(NginxConfigStatementTest, ToString) {
+    NginxConfigStatement statement;
+    statement.tokens_.push_back("foo");
+    statement.tokens_.push_back("bar");
+    EXPECT_EQ("foo bar;\n", statement.ToString(0));
+}
+
+class NginxStringConfigTest : public ::testing::Test {
+    protected:
+    bool ParseString(const std::string& config_string) {
+        std::stringstream config_stream(config_string);
+        return parser_.Parse(&config_stream, &out_config_);
+    }
+    NginxConfigParser parser_;
+    NginxConfig out_config_;
+};
+
+TEST_F(NginxStringConfigTest, SimpleConfig) {
+    EXPECT_TRUE(ParseString("foo bar;"));
+    EXPECT_EQ(1, out_config_.statements_.size());
+    EXPECT_EQ(2, out_config_.statements_[0]->tokens_.size());
+}
+
+TEST_F(NginxStringConfigTest, DoubleNestedConfig) {
+    EXPECT_TRUE(ParseString("foo { bar { baz huz; } }"));
+} 
+
